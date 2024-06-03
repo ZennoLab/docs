ad114f013f639ac39c1ad2f608de3009e3602484
Andrei Kolesnikov <samwin230@gmail.com>
2024-06-04 00:58:05 +0300

    Another Commit

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+my_display = {'1920x1080', '2560x1440', '3480x2160'}
+mobile_display = {'1920x1080', '1260x720', '920x600'}
 
+print(my_display.intersection(mobile_display))
+print(mobile_display.union(my_display))
+print(mobile_display.issubset(my_display))
