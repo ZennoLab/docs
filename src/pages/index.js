e4fa6fecaa29963b03690819c2a5951494a69894
Vladimir Abramiants <vladimir.abramiants@zennolab.com>
2025-09-23 15:26:50 +0300

    feat: [ZDOC-78] Added meta tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Layout from '@theme/Layout';
+import Head from '@docusaurus/Head';
 import { Banner, Products, UsefulLinks } from '@site/src/components';
 
 
@@ -7,6 +8,10 @@ export default function Home() {
     <Layout
       title={`Документация ZennoLab`}
       description="Самый простой способ автоматизировать без знаний кода">
+      <Head>
+        <meta name="google-site-verification" content="bAeMEZMIwgWa0nQ-UsAdRWNewH4o9ynLQui-iXoJruc" />
+        <meta name="yandex-verification" content="e5efd80d7516472d" />
+      </Head>
       <Banner />
       <main>
         <Products />
