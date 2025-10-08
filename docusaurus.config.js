ef7051ff104dd19e16e54f88326dc8b5356bd557
Vladimir Abramiants <vladimir.abramiants@zennolab.com>
2025-10-08 18:20:47 +0300

    feat: [ZDOC-91] Changed zennolab docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,4 +1,4 @@
-// @ts-check
+// @ts-nocheck
 // `@type` JSDoc annotations allow editor autocompletion and type checking
 // (when paired with `@ts-check`).
 // There are various equivalent ways to declare your Docusaurus config.
@@ -18,17 +18,6 @@ const config = {
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
-  // Добавляем мета-тег для отключения индексации всех поисковиков
-  headTags: [
-    {
-      tagName: 'meta',
-      attributes: {
-        name: 'robots',
-        content: 'noindex, nofollow, nosnippet, noarchive',
-      },
-    },
-  ],
-
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'zennolab', // Usually your GitHub org/user name.
@@ -42,12 +31,6 @@ const config = {
   i18n: {
     defaultLocale: 'ru',
     locales: ['en', 'ru'],
-  },
-
-  markdown: {
-    hooks: {
-      onBrokenMarkdownLinks: 'warn',
-    },
   },
 
   presets: [
@@ -156,7 +139,17 @@ const config = {
             to: '/zennoproxy/category/getting-started/',
             from: ['/zennoproxy/basics/getting-started', '/zennoproxy/intro'],
           },
+          {
+            to: '/',
+            from: ['/index', '/index/'],
+          }
         ],
+        createRedirects(existingPath) {
+          return [
+            existingPath.replace('/?', '/'),
+            existingPath.replace('//////', '/'),
+          ]
+        }
       }
     ],
   ],
