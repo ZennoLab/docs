192e439b955d0cd14971119d4157300a0bcb583c
djaga <83035465+djaga-ru@users.noreply.github.com>
2026-02-21 20:37:17 +0300

    fix: set trailingSlash: false to fix locale switching for versioned pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,6 +24,7 @@ const config = {
   projectName: 'docs', // Usually your repo name.
 
   onBrokenLinks: 'warn',
+  trailingSlash: false,
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
